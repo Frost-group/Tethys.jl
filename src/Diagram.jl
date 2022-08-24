395e66600744023156b2479ede3d1a110bd96adb
wenzhaoren <2456767175@qq.com>
2022-08-24 14:48:53 +0100

    added test function to check the sequence of the time segment; insert and delete update with the index of time segment; added the delete arc update(only consider arc containing no arc)

--- src/Diagram.jl
+++ src/Diagram.jl
@@ -28,7 +28,7 @@ mutable struct Diagram
         τ = rand()*max_τ
         p_grid=collect(LinRange(0, p_max, grid_num))
         p_x=rand(p_grid)
-        new(mass, μ, ω, α, [p_x,0,0], p_grid, τ, max_τ, 0, max_order, [], [Line([p_x,0,0], [0, τ], mass, μ)], p_ins, p_rem)
+        new(mass, μ, ω, α, [p_x,0,0], p_grid, τ, max_τ, 0, max_order, [], [Line([p_x,0,0], [0, τ], mass, μ, 1)], p_ins, p_rem)
     end
 
 end
@@ -41,8 +41,8 @@ mutable struct Arc <:Propagator
     q :: Array{Float64}
     period :: Array{Float64}
     ω :: Float64
-    k_in :: Array{Float64}
-    k_out :: Array{Float64}
+    index_in :: Int64
+    index_out :: Int64
 end
 
 
@@ -51,6 +51,7 @@ mutable struct Line <:Propagator
     period :: Array{Float64}
     mass::Float64
     μ::Float64
+    index::Int64
 end
 
 function green_zero(diagram::Diagram)
@@ -131,7 +132,7 @@ function insert_arc!(diagram::Diagram)
     α_squared=2pi*α*sqrt(2)
 
     if order+1>diagram.max_order
-        return
+        return false
     end
 
     line_box=diagram.line_box
@@ -151,17 +152,17 @@ function insert_arc!(diagram::Diagram)
     arc_T=τ_2-τ_1
 
     q=rand(Normal(0,sqrt(m/arc_T)),3)
-    new_arc=Arc(q,[τ_1,τ_2],ω,line.k,line.k)
+    new_arc=Arc(q,[τ_1,τ_2],ω,index,index+2)
 
     time=[τ_L,τ_1,τ_2,τ_R]
-    k_box=[line.k,q,line.k]
+    k_box=[line.k,line.k-q,line.k]
     line_to_add=[]
 
     w_x=green_zero(line)
     w_y=phono_propagator(new_arc)*α_squared/(2*pi)^3
 
     for i in 1:3
-        new_line=Line(k_box[i] ,[time[i],time[i+1]], m, μ)
+        new_line=Line(k_box[i] ,[time[i],time[i+1]], m, μ, index+i-1)
         push!(line_to_add,new_line)
         w_y*=green_zero(new_line)
     end
@@ -169,24 +170,156 @@ function insert_arc!(diagram::Diagram)
     p_x_y=diagram.p_ins/(τ_R-τ_L)*ω*exp(-ω*arc_T)
     p_x_y*=exp(-norm(q)^2/(2m)*arc_T)/(2pi*m/arc_T)^1.5
 
-    p_y_x=diagram.p_rem/(length(diagram.arc_box)+1)
+    p_y_x=diagram.p_rem/(order+1)
     
     r=w_y*p_y_x/(w_x*p_x_y)
-    println("r=",r)
+    # println("insert_r=",r)
 
     if r<rand()
         return false
     else
         diagram.order+=1
-        push!(diagram.arc_box,new_arc)
         deleteat!(line_box, index)
 
         for i in 1:3
             insert!(line_box, index, line_to_add[4-i])
         end
+        # println("insert index is ",[index,index+2])
+        if length(line_box)>=index+3
+            for i in index+3:length(line_box)
+                line_box[i].index=i
+            end
+        end
+
+        for arc in diagram.arc_box
+            if arc.index_out<=index
+                continue
+            elseif arc.index_in>=index
+                arc.index_in+=2
+                arc.index_out+=2
+            elseif arc.index_in<index && arc.index_out>index
+                arc.index_out+=2
+            end
+        end
+
+        push!(diagram.arc_box,new_arc)
 
         return true
     end
+end
+
+function remove_arc!(diagram::Diagram)
+
+    order=diagram.order
+    m=diagram.mass
+    μ=diagram.μ
+    ω=diagram.ω
+    α=diagram.α
+    α_squared=2pi*α*sqrt(2)
+
+    if order-1<1
+        return false
+    end
+    
+    arc_box=diagram.arc_box
+    index=rand(1:length(arc_box))
+    arc=arc_box[index]
+    index_in=arc.index_in
+    index_out=arc.index_out
+    q=arc.q
+
+    if index_out-index_in>2
+        return false
+    end
+
+    line_box=diagram.line_box
+    line_in=line_box[index_in]
+    line_out=line_box[index_out]
+    line_to_rem=[line_box[index_in],line_box[index_in+1],line_box[index_in+2]]
+
+    τ_L=line_in.period[1]
+    τ_R=line_out.period[2]
+    new_line=Line(line_in.k ,[τ_L,τ_R], m, μ, index_in)
+
+    w_x=green_zero(new_line)
+    w_y=phono_propagator(arc)*α_squared/(2*pi)^3
+
+    for i in 1:3
+        w_y*=green_zero(line_to_rem[i])
+    end
+
+    τ_1=copy(arc.period[1])
+    τ_2=copy(arc.period[2])
+    arc_T=τ_2-τ_1
+    p_x_y=diagram.p_ins/(τ_R-τ_L)*ω*exp(-ω*arc_T)
+    p_x_y*=exp(-norm(q)^2/(2m)*arc_T)/(2pi*m/arc_T)^1.5
+
+    p_y_x=diagram.p_rem/order
+
+    r=(w_x*p_x_y)/(w_y*p_y_x)
+    # println("remove_r=",r)
+
+    if r<rand()
+        return false
+    else
+        diagram.order-=1
+        
+        deleteat!(arc_box, index)
+        deleteat!(line_box, index_in:index_out)
+        insert!(line_box, index_in, new_line)
+
+        if length(line_box)>=index_in+1
+            for i in index_in+1:length(line_box)
+                line_box[i].index=i
+            end
+        end
+
+        for arc in diagram.arc_box
+            if arc.index_out<=index_in
+                continue
+            elseif arc.index_in>=(index_in+2)
+                arc.index_in-=2
+                arc.index_out-=2
+            elseif arc.index_in<index_in && arc.index_out>index_in
+                arc.index_out-=2
+            # elseif arc.index_in>index_in && arc.index_out<index_out
+            #     arc.index_in-=1
+            #     arc.index_out-=1
+            end
+        end
+
+        return true
+    end
+end
+
+function check_index(diagram::Diagram)
+    index_box=[]
+    for index in 1:length(diagram.line_box)
+        line=diagram.line_box[index]
+        append!(index_box,line.index)
+    end
+
+    println("index_box",index_box)
+end
+
+function check_arcindex(diagram::Diagram)
+    index_box=[]
+    for index in 1:length(diagram.arc_box)
+        arc=diagram.arc_box[index]
+        push!(index_box,[arc.index_in,arc.index_out])
+    end
+
+    println("index_box",index_box)
+end
+
+function check_timeorder(diagram::Diagram)
+    index_box=[]
+    for index in 1:length(diagram.line_box)
+        line=diagram.line_box[index]
+        push!(index_box,[line.period[1],line.period[2]])
+    end
+
+    println("index_box",index_box)
 end
 
 begin
@@ -203,10 +336,30 @@ begin
 end
 
 begin
+    println("begin")
+    diagram_a=Diagram(p_max, max_τ, max_order, mass, μ, ω, α)
     loop=100
     for i in 1:loop
-        insert_arc!(diagram_a)
-        println("order is ",diagram_a.order)
+
+        if insert_arc!(diagram_a)
+            println("insert")
+            check_index(diagram_a)
+            # check_arcindex(diagram_a)
+            # check_timeorder(diagram_a)
+        end
+        if remove_arc!(diagram_a)
+            println("remove")
+            # check_arcindex(diagram_a)
+            check_index(diagram_a)
+            # check_timeorder(diagram_a)
+        end
+
+        # if insert_arc!(diagram_a)
+        #     check_arcindex(diagram_a)
+        #     check_timeorder(diagram_a)
+        #     println("order is ",diagram_a.order)
+        # end
+        # println("order is ",diagram_a.order)
     end
 end
 
