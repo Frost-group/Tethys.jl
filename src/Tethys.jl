796966eaf980d1e8d82f5db93161df4df4811960
wenzhaoren <2456767175@qq.com>
2022-10-29 16:30:36 +0100

    separated useful updates from useless ones;implemented temporary data storage;test for alpha scanning and k scanning

--- src/Tethys.jl
+++ src/Tethys.jl
@@ -8,7 +8,15 @@ println("Loading Tethys...")
 
 greet() = println("Hello Polarons! ♓")
 
+export insert_arc!, remove_arc!, swap_arc!, extend!
+export Diff_more, Diff_2, Diagram, Line, Arc
+export hist_measure!, Hist_Record
+
 include("BareExpansion.jl")
+include("update.jl")
+include("measure.jl") # Potentials for QMC algorithms.
+include("Diagram.jl") # PIMC moves.
+
 
 greet()
 
