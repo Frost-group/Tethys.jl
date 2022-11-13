051a45d0685f8ee73462c0966b980ff63ab5577b
unknown <darylkylee@gmail.com>
2022-11-13 16:16:05 +0000

    added loop time evaluation

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -46,3 +46,26 @@ begin
     @info "Diagram Order: "*string(diagram.order)
     t = @benchmark insert_arc!(diagram,regime)
 end
+
+function measure_loop(α, μ, p=0)
+    n_loop=10
+    num_mea=1; regime=Diff_more();
+    max_τ=30; max_order=500; mass=1; ω=1;
+
+
+    linear(t, p) = p[1].-p[2].*t
+    bin_width=max_τ/300
+    min_time=Int(div(5,bin_width,RoundUp))
+    max_time=Int(div(12,bin_width,RoundUp))
+
+    directory = "F://data"
+    store_data = false
+    hist=Hist_Record(300,max_τ,max_order)
+    diagram=Diagram(p, max_τ, max_order, mass, μ, ω, α)
+    diagram,hist,green_record,zero_record,green_func,variance=hist_measure!(diagram,hist,directory,n_loop,store_data,1000)
+    
+end
+
+begin
+    t = @benchmark measure_loop(5.0, -6)
+end
